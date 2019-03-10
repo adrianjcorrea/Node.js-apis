17f583af1fd4fbaa84b90a10144881836d444f29
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-03-09 23:48:58 -0600

    API post request

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const users = {
  best: 'football'
 }
 // I will be incrementing max users Id as i add a user.
-let max_users_id = 0;
+let max_user_id = 1;
+// Function lets us use plugin bodyParser to post a user.
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.bodyParser());
 
 // Api call Get request.
 server.get("/", function(req, res, next){
@@ -18,6 +21,19 @@ server.get("/", function(req, res, next){
   // Return a response users object as a string.
   res.end(JSON.stringify(users));
   // Ensure the Ends of our req-res cycle.
+  return next();
+});
+// Adding users using post requests
+server.post("/users", function(req, res, next){
+  // Parameters coming in will define our new user.
+  let user = req.params;
+  // Increment our max_user_id and set max_user_id as the user id.
+  max_user_id++;
+  user.id = max_user_id;
+  users[user.id] = user;
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(user));
   return next();
 });
 
